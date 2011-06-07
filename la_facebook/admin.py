799effb8009cfa679743142620bc42e62c3fe45a
Shoe <schomaker.c@gmail.com>
2011-06-07 18:03:56 -0400

    Add friends to admin

--- la_facebook/admin.py
+++ la_facebook/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
-from la_facebook.models import UserAssociation
+from la_facebook.models import Friend, UserAssociation
 
 if settings.DEBUG:
     admin.site.register(UserAssociation)
+    admin.site.register(Friend)
