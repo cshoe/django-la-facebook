18e3ed10054a92116642942a86ef8894318d4e29
Shoe <schomaker.c@gmail.com>
2011-06-08 14:02:45 -0400

    Get friends internally is now available in get_friends_on_site

--- la_facebook/utils/graph_api.py
+++ la_facebook/utils/graph_api.py
@@ -1,32 +1,44 @@
-import json, urllib, urllib2
+import json, facebook, urllib, urllib2
 
-import facebook
+from django.conf import settings
 
-from la_facebook.models import UserAssociation
+from la_facebook.models import Friend, UserAssociation
+from la_facebook.la_fb_logging import logger
+FACEBOOK_SETTINGS = getattr(settings, 'FACEBOOK_ACCESS_SETTINGS', {})
 
 def get_friends_on_site(user):
-    """
-    Find FB friends that are also using the site. Only works for the currently
-    logged in user.
-    
-    Raises UserAssociation.DoesNotExist and facebook.GraphAPIError
-    """
-    
-    fb_friends = get_friends_on_facebook(user)
-    print 'asdf: {0}'.format(fb_friends)
-    
-    if fb_friends:
-        # even though the FQL query says that we should only have user id's that
-        # are associated with our app, we're still going to filter against our
-        # data in case there is some breakage (FB has them but we don't) 
-        site_friends = UserAssociation.objects.filter(identifier__in=[i['uid'] for i in fb_friends])
-        return site_friends
+    """ Find FB friends that are also using the site. """
+    try:
+        get_friends_internally = FACEBOOK_SETTINGS['STORE_FRIENDS']
+    except KeyError:
+        get_friends_internally = False
+
+    if get_friends_internally:
+        logger.debug('Getting friends internally for {0}'.format(user))
+        friend_ids_on_site = Friend.objects.filter(uid1=user).values_list('uid2', flat=True)
+        friend_user_associations = UserAssociation.objects.filter(user__pk__in=friend_ids_on_site).all()
+        return friend_user_associations
+    else:
+        logger.debug('Getting friends from Facebook for {0}'.format(user))
+        # going to FB for friendships.
+        fb_friends = get_friends_on_facebook(user)
+        if fb_friends:
+            # even though the FQL query says that we should only have user id's that
+            # are associated with our app, we're still going to filter against our
+            # data in case there is some breakage (FB has them but we don't) 
+            site_friends = UserAssociation.objects.filter(identifier__in=[i['uid'] for i in fb_friends])
+            return site_friends
     return None
 
 def get_friends_on_facebook(user):
-    #raises UserAssociation.DoesNotExist:
-    # TODO: NEED ERROR CHECKING
-    assoc = UserAssociation.objects.get(user=user)
+    """
+    Go to Facebook to find a user's friends that have registered with the app.
+    """
+    try:
+        assoc = UserAssociation.objects.get(user=user)
+    except UserAssociation.DoesNotExist:
+        logger.debug('No UserAssociation for {0}'.format(user))
+        return None
     
     fql_query = "SELECT uid FROM user WHERE is_app_user AND uid IN (SELECT uid2 FROM friend WHERE uid1 = {0})".format(assoc.identifier)
     fb_friends = json.load(do_fql_query(fql_query, assoc.token))
@@ -60,6 +72,7 @@ def do_fql_query(query, token=None, format='JSON'):
     req = urllib2.Request(url)
     response = opener.open(req)
 
+    # let error responses out so that other functions can handle them in their own way?
     if response is not None:
         return response
     return
